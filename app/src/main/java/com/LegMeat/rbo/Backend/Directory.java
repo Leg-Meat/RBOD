9fca63281e29080008e8e8dfcb838f4ba3ae1655
Leg Meat <198740067+Leg-Meat@users.noreply.github.com>
2026-02-01 01:40:56 +0000

    more touch ups

--- app/src/main/java/com/LegMeat/rbo/Backend/Directory.java
+++ app/src/main/java/com/LegMeat/rbo/Backend/Directory.java
@@ -16,36 +16,6 @@ public class Directory {
     private ArrayList<Video> videoList = new ArrayList<>();
     private File directoryFolder;
 
-
-    public void originalExecuteCuts() {
-        // find base directory of all videos
-        String newDirectory = directoryFolder + " - Overlap Removed";
-        // Create a new directory for cut videos
-        try {
-            Files.createDirectory(Paths.get(newDirectory));
-            int videoNum = 1;
-            for (Video video : this.videoList) {
-                System.out.println("Attempting to cut video " + videoNum + " out of " + videoList.size() + " .");
-                if (video.isCorrupted()) {
-                    System.out.println("Video " + videoNum + " is corrupted.");
-                } else {
-                    try {
-                        video.originalcopyCut(newDirectory);
-                        System.out.println("Successfully cut video " + videoNum + "!");
-                    } catch (InvalidFileException e) {
-                        System.out.println("Video has no cutpoint yet.");
-                    } catch (IOException e) {
-                        System.out.println("IO Error cutting or overwriting video " + videoNum + "! Ensure ffmpeg is" +
-                                "installed to system path and has write permissions. Ensure File.nio.file.Files has " +
-                                "overwrite permissions. Origianl message: " + e.getMessage());
-                    }
-                }
-                videoNum++;
-            }
-        } catch (IOException e) {
-            System.out.println("Could not create new file directory. 'Cut' directory already exists!");
-        }
-    }
     /**
      * When the application is ready, this method will execute all the video cuts, according to each individul videos
      * cut points.
